8a4968257a7bd35f386bb7f1ae5deb17500660e1
Donovan <me.bigd@gmail.com>
2017-08-11 18:24:36 +1000

    Initial Commit

---
+++ app/User.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App;
+
+use Illuminate\Auth\Authenticatable;
+use Laravel\Lumen\Auth\Authorizable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+
+class User extends Model implements AuthenticatableContract, AuthorizableContract
+{
+    use Authenticatable, Authorizable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'username', 'steamid', 'key', 'avatar', 'trade_link', 'inv_updated', 'name', 'email'
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token', 'admin',
+    ];
+
+    public function trades()
+    {
+        return $this->hasMany('App\Trades', 'user_id');
+    }
+}
