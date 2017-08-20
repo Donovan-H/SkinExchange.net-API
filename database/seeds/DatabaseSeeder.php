3b13b524936da7681d4fbc517c040b3ec89869ae
Donovan <me.bigd@gmail.com>
2017-08-20 15:10:36 +1000

    Proxy seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -22,5 +22,56 @@ class DatabaseSeeder extends Seeder
         DB::table('item_exterior')->insert([
             'exterior' => 'None'
         ]);
+/*
+        DB::table('proxies')->insert([
+            'ip_address' => '104.149.86.34',
+            'username' => 'sn9obgspxxk',
+            'password' => 'e98a7180a0833c9916d'
+        ]);
+        DB::table('proxies')->insert([
+            'ip_address' => '108.175.52.186',
+            'username' => 'sn9obgspxxk',
+            'password' => 'e98a7180a0833c9916d'
+        ]);
+        DB::table('proxies')->insert([
+            'ip_address' => '104.149.91.220',
+            'username' => 'sn9obgspxxk',
+            'password' => 'e98a7180a0833c9916d'
+        ]);
+        DB::table('proxies')->insert([
+            'ip_address' => '108.175.52.22',
+            'username' => 'sn9obgspxxk',
+            'password' => 'e98a7180a0833c9916d'
+        ]);
+        DB::table('proxies')->insert([
+            'ip_address' => '107.183.242.245',
+            'username' => 'sn9obgspxxk',
+            'password' => 'e98a7180a0833c9916d'
+        ]);
+        DB::table('proxies')->insert([
+            'ip_address' => '109.73.79.176',
+            'username' => 'sn9obgspxxk',
+            'password' => 'e98a7180a0833c9916d'
+        ]);
+        DB::table('proxies')->insert([
+            'ip_address' => '109.73.79.157',
+            'username' => 'sn9obgspxxk',
+            'password' => 'e98a7180a0833c9916d'
+        ]);
+        DB::table('proxies')->insert([
+            'ip_address' => '103.197.169.53',
+            'username' => 'sn9obgspxxk',
+            'password' => 'e98a7180a0833c9916d'
+        ]);
+        DB::table('proxies')->insert([
+            'ip_address' => '104.149.91.217',
+            'username' => 'sn9obgspxxk',
+            'password' => 'e98a7180a0833c9916d'
+        ]);
+        DB::table('proxies')->insert([
+            'ip_address' => '104.149.91.194',
+            'username' => 'sn9obgspxxk',
+            'password' => 'e98a7180a0833c9916d'
+        ]);*/
     }
 }
