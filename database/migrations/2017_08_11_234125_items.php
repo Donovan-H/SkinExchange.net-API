341ca9b8743d55c6c25b64643dd49a7171810de1
Donovan Hare <haredj@mail.com>
2017-09-28 11:44:47 +0100

    Prefixed csgo tables

--- database/migrations/2017_08_11_234125_items.php
+++ database/migrations/2017_08_11_234125_items.php
@@ -13,7 +13,7 @@ class Items extends Migration
      */
     public function up()
     {
-        Schema::create('items', function (Blueprint $table) {
+        Schema::create('csgo_items', function (Blueprint $table) {
             $table->increments('item_id_pk');
             $table->integer('type_id_fk')->unsigned();
             $table->integer('weapon_id_fk')->unsigned();
@@ -30,39 +30,39 @@ class Items extends Migration
             $table->timestamps();
         });
 
-        Schema::create('item_type', function (Blueprint $table) {
+        Schema::create('csgo_item_type', function (Blueprint $table) {
             $table->increments('type_id_pk');
             $table->string('type', 128);
         });
 
-        Schema::create('item_weapon', function (Blueprint $table) {
+        Schema::create('csgo_item_weapon', function (Blueprint $table) {
             $table->increments('weapon_id_pk');
             //$table->integer('type_id_fk')->unsigned();//IMPLEMENT
             $table->string('weapon', 128);
         });
 
-        Schema::create('item_collection', function (Blueprint $table) {
+        Schema::create('csgo_item_collection', function (Blueprint $table) {
             $table->increments('collection_id_pk');
             $table->string('collection', 128);
         });
 
-        Schema::create('item_category', function (Blueprint $table) {
+        Schema::create('csgo_item_category', function (Blueprint $table) {
             $table->increments('category_id_pk');
             $table->string('category', 128);
         });
 
-        Schema::create('item_quality', function (Blueprint $table) {
+        Schema::create('csgo_item_quality', function (Blueprint $table) {
             $table->increments('quality_id_pk');
             $table->string('quality', 128);
             $table->string('color', 8);
         });
 
-        Schema::create('item_exterior', function (Blueprint $table) {
+        Schema::create('csgo_item_exterior', function (Blueprint $table) {
             $table->increments('exterior_id_pk');
             $table->string('exterior', 128);
         });
 
-        Schema::table('items', function (Blueprint $table) {
+        Schema::table('csgo_items', function (Blueprint $table) {
             $table->foreign('type_id_fk')->references('type_id_pk')->on('item_type');
             $table->foreign('weapon_id_fk')->references('weapon_id_pk')->on('item_weapon');
             $table->foreign('collection_id_fk')->references('collection_id_pk')->on('item_collection');
@@ -79,12 +79,12 @@ class Items extends Migration
      */
     public function down()
     {
-        Schema::drop('items');
-        Schema::drop('item_type');
-        Schema::drop('item_weapon');
-        Schema::drop('item_collection');
-        Schema::drop('item_category');
-        Schema::drop('item_quality');
-        Schema::drop('item_exterior');
+        Schema::drop('csgo_items');
+        Schema::drop('csgo_item_type');
+        Schema::drop('csgo_item_weapon');
+        Schema::drop('csgo_item_collection');
+        Schema::drop('csgo_item_category');
+        Schema::drop('csgo_item_quality');
+        Schema::drop('csgo_item_exterior');
     }
 }
